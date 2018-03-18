0a7d186cc8576c41a8c30f6fc7f31e6e0ec22943
Vasiliy <vasiliy.t@ezscratch.com>
2018-03-18 23:41:42 +0200

    fix positions
    Signed-off-by: Vasiliy <vasiliy.t@ezscratch.com>

--- server/db/fixtures/devices.js
+++ server/db/fixtures/devices.js
@@ -3,21 +3,21 @@ module.exports = [
     "id": 1,
     "user_id": 1,
     "name": "pc_device",
-    "latitude": 50.6027219,
-    "longitude": 26.397014199999997,
+    "latitude": 50.3027219,
+    "longitude": 26.697014199999997,
   },
   {
     "id": 2,
     "user_id": 1,
     "name": "iPod",
-    "latitude": 51.6027219,
-    "longitude": 27.397014199999997,
+    "latitude": 51.0127219,
+    "longitude": 25.997014199999997,
   },
   {
     "id": 3,
     "user_id": 1,
     "name": "iPad",
-    "latitude": 49.6027219,
-    "longitude": 25.397014199999997,
+    "latitude": 50.8027219,
+    "longitude": 26.117014199999997,
   }
 ]
