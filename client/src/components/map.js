1aeb77562738426e5a03cc34bf76ca26c4fbc3c4
Vasiliy <vasiliy017@gmail.com>
2018-03-20 13:12:09 +0200

    device markers
    Signed-off-by: Vasiliy <vasiliy017@gmail.com>

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -12,6 +12,44 @@ import { connect } from 'react-redux';
 import { requestSetDevicePosition } from '../actions/devices_actions';
 
 
+class DeviceMarker extends React.Component {
+  
+  state = {
+    showInfo: true
+  }
+
+  handleClick = () => {
+    this.setState({ showInfo: true })
+  }
+  handleCloseInfoClick = () => {
+    this.setState({ showInfo: false })
+  }
+
+  render() {
+    console.log('---DeviceMarker.render:', this.props)
+    let { id, latitude, longitude, name, currentDeviceId, focusDeviceId } = this.props;
+    let zIndex = 1;
+    let infoStyle = {}
+    if (id == currentDeviceId) {
+      infoStyle = { fontWeight: 'bold' }
+      zIndex = 2;
+    }
+    if (id == focusDeviceId) {
+      infoStyle = { ...infoStyle, backgroundColor: `yellow`, padding: '5px' }
+      zIndex = 3;
+    }
+    return (
+      <Marker key={id} position={{ lat: latitude, lng: longitude }} title={name} onClick={this.handleClick} zIndex={zIndex} >
+        {this.state.showInfo && (
+          <InfoWindow onCloseClick={this.handleCloseInfoClick}>
+            <div style={infoStyle}>{name}</div>
+          </InfoWindow>
+        )}
+      </Marker>
+    )
+  }
+}
+
 const GMap = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAWXZwnl9PDC6_pu6I6vfODtuJ3BipQibY&v=3.exp&libraries=geometry,drawing,places",
@@ -22,25 +60,11 @@ const GMap = compose(
   withScriptjs,
   withGoogleMap,
 )((props) =>
-  <GoogleMap zoom={props.zoom} center={props.center}>
-    {props.markers.map((marker, index) =>
-      <Marker key={index} position={marker.position} onClick={props.onMarkerClick} />
-    )}
+  <GoogleMap {...props.mapProps}>
     {props.devices.map((device, index) => {
       if (device.latitude) {
-        let infoStyle = {}
-        if (device.id == props.currentDeviceId) {
-          infoStyle = { fontWeight: 'bold' }
-        }
-        if (device.id == props.focusDeviceId) {
-          infoStyle = { ...infoStyle, backgroundColor: `yellow`, padding: '5px' }
-        }
         return (
-          <Marker key={device.id} position={{ lat: device.latitude, lng: device.longitude }} onClick={props.onMarkerClick} title={device.name} >
-            <InfoWindow>
-              <div style={infoStyle}>{device.name}</div>
-            </InfoWindow>
-          </Marker>
+          <DeviceMarker key={device.id} {...device} currentDeviceId={props.currentDeviceId} focusDeviceId={props.focusDeviceId}/>
         )
       }
     }
@@ -54,14 +78,29 @@ class Map extends React.PureComponent {
     super(props);
 
     this.state = {
-      zoom: 8,
-      isMarkerShown: true,
-      center: this.getCunter(props),
-      markers: [],
+      mapProps: {
+        zoom: 8,
+        center: this.getDefaultCenter(props),
+      },
     }
   }
 
-  getCunter(props) {
+  setCenter(coords) {
+    console.log('---setCenter:', coords)
+
+    let center = {
+      lat: coords.latitude,
+      lng: coords.longitude
+    }
+    this.setState({
+      mapProps: {
+        ...this.state.mapProps,
+        center: center
+      }
+    })
+  }
+
+  getCenter(props) {
     let center = null;
     let { list, currentDeviceId, focusDeviceId } = props.devices
     list.map(device => {
@@ -74,7 +113,19 @@ class Map extends React.PureComponent {
         }
       }
     })
-    console.log('---getCunter:', center)
+    console.log('---getCenter:', center)
+    return center
+  }
+
+  getDefaultCenter(props) {
+    let center = this.getCenter(props);
+    if (!center) {
+      let { list } = props.devices
+      if (list.length) {
+        center = { lat: list[0].latitude, lng: list[0].longitude }
+      }
+    }
+    console.log('---getDefaultCunter:', center)
     return center
   }
 
@@ -84,68 +135,77 @@ class Map extends React.PureComponent {
 
   componentDidMount() {
     console.log('---Map.componentDidMount:', this.props)
-    this.delayedShowMarker()
-    this.getCurrentPosition()
+    //this.updateCurrentPosition()
+    this.getCurrentPosition().then(position => {
+      console.log('---setDefaultCunter in getCurrentPosition:', position.coords)
+      this.setCenter(position.coords)
+      /*
+      if (this.props.currentDeviceId) {
+          let device = {
+            id: this.props.currentDeviceId,
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          }
+          console.log('---updateCurrentPosition:', device)
+          this.props.setDevicePosition(device)
+      }
+      */
+    })
+    this.subscribeWatchPosition();
   }
 
   componentWillReceiveProps(nextProps) {
     console.log('---Map.componentWillReceiveProps:', nextProps)
-    this.setState({
-      center: this.getCunter(nextProps)
-    });
+    let newCenter = this.getCenter(nextProps)
+    if (newCenter) {
+      //this.setState({ mapProps: { ...this.state.mapProps, center: newCenter } })
+    }
     if (nextProps.currentDeviceId != this.props.currentDeviceId) {
       this.updateCurrentPosition();
     }
   }
-
-  delayedShowMarker = () => {
-    setTimeout(() => {
-      this.setState({ isMarkerShown: true })
-    }, 3000)
-  }
-
-  handleMarkerClick = () => {
-    if (this.state.zoom < 18) {
-      this.setState({ zoom: this.state.zoom + 1 })
-    }
-  }
+  
   getCurrentPosition = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((function (position) {
-        var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
-
-        console.log('---position:', pos)
-        //infoWindow.setPosition(pos);
-        //infoWindow.setContent('Location found.');
-        //map.setCenter(pos);
-        //this.setState({ center: pos, markers: [{ position: pos }] })
-        //this.setState({ center: pos, markers: [{ position: pos }] })
-        if (this.props.currentDeviceId) {
-          let device = {
-            id: this.props.currentDeviceId,
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-          }
-          this.props.setDevicePosition(device)
-        }
-      }).bind(this), function (err) {
-        console.warn('---error:')
-        console.warn(err)
-        //handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      console.warn('---error: Browser does not support Geolocation')
-      //handleLocationError(false, infoWindow, map.getCenter());
-    }
+    return new Promise(function (resolve, reject) {
+      try {
+        navigator.geolocation.getCurrentPosition((function (position) {
+          console.log('---getCurrentPosition: ', position)
+          resolve(position)
+        }).bind(this), function (geo_error) {
+          reject(geo_error)
+          console.error('---getCurrentPosition error: ', geo_error)
+        });
+      } catch (e) {
+          reject(e)
+          console.error('---getCurrentPosition error: ', geo_error)
+      }
+    })
   }
 
   updateCurrentPosition = () => {
+    if (this.props.currentDeviceId) {
+      this.getCurrentPosition().then(pos => {
+        let device = {
+          id: this.props.currentDeviceId,
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+        console.log('---updateCurrentPosition:', device)
+        this.props.setDevicePosition(device)
+      })
+    }
+  }
+
+  subscribeWatchPosition = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((function (position) {
-        console.log('---updateCurrentPosition:', position.coords)
+      var watchOptions = {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 30000
+      };
+      //navigator.geolocation.clearWatch(watchID);
+      var watchID = navigator.geolocation.watchPosition((function (position) {
+        console.log('---watchPosition:', position.coords)
         if (this.props.currentDeviceId) {
           let device = {
             id: this.props.currentDeviceId,
@@ -155,8 +215,8 @@ class Map extends React.PureComponent {
           this.props.setDevicePosition(device)
         }
       }).bind(this), function (err) {
-        console.warn('---updateCurrentPosition error:', err)
-      });
+        console.warn('---watchPosition error:', err)
+      }, watchOptions);
     }
   }
 
@@ -165,11 +225,7 @@ class Map extends React.PureComponent {
     console.warn('---Map: render->state: ', this.state);
     return (
       <GMap
-        isMarkerShown={this.state.isMarkerShown}
-        onMarkerClick={this.handleMarkerClick}
-        center={this.state.center}
-        markers={this.state.markers}
-        zoom={this.state.zoom}
+        mapProps={this.state.mapProps}
         devices={this.props.devices.list}
         currentDeviceId={this.props.devices.currentDeviceId}
         focusDeviceId={this.props.devices.focusDeviceId}
