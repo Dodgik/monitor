02717103b5ddc0560126264b2692f54a4934db00
Vasiliy <vasiliy017@gmail.com>
2018-04-18 17:11:57 +0300

    refactor map component
    Signed-off-by: Vasiliy <vasiliy017@gmail.com>

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -4,49 +4,11 @@ import {
     withScriptjs,
     withGoogleMap,
     GoogleMap,
-    Marker,
-    InfoWindow,
 } from 'react-google-maps';
 import { connect } from 'react-redux';
+import DeviceMarker from './DeviceMarker';
 
 
-class DeviceMarker extends React.Component {
-  
-  state = {
-    showInfo: true
-  }
-
-  handleClick = () => {
-    this.setState({ showInfo: true })
-  }
-  handleCloseInfoClick = () => {
-    this.setState({ showInfo: false })
-  }
-
-  render() {
-    console.log('---DeviceMarker.render:', this.props)
-    let { id, latitude, longitude, name, currentDeviceId, focusDeviceId } = this.props;
-    let zIndex = 1;
-    let infoStyle = {}
-    if (id == currentDeviceId) {
-      infoStyle = { fontWeight: 'bold' }
-      zIndex = 2;
-    }
-    if (id == focusDeviceId) {
-      infoStyle = { ...infoStyle, backgroundColor: `yellow`, padding: '5px' }
-      zIndex = 3;
-    }
-    return (
-      <Marker key={id} position={{ lat: latitude, lng: longitude }} title={name} onClick={this.handleClick} zIndex={zIndex} >
-        {this.state.showInfo && (
-          <InfoWindow onCloseClick={this.handleCloseInfoClick} zIndex={zIndex}>
-            <div style={infoStyle}>{name}</div>
-          </InfoWindow>
-        )}
-      </Marker>
-    )
-  }
-}
 
 
 const GMap = compose(
